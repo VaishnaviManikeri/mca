8c692728e11076da786e5e082efe2940e2e815bb
Vaishnavi Manikeri <vaishnavimanikeri@gmail.com>
2026-02-25 14:52:15 +0530

    renamed auth

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { loginAdmin, getDashboard } = require('../controllers/adminController');
 const { protect } = require('../middleware/authMiddleware');
-
 router.post('/login', loginAdmin);
 router.get('/dashboard', protect, getDashboard);
 
