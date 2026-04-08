f861f5a9c6e6d2b24f2b7f61e7b6c6fde2c9ced4
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-04-08 16:27:50 +0530

    Add blog and admission routes
    Added blog and admission routes to the server.

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use('/api/gallery', require('./routes/galleryRoutes'));
 app.use('/api/announcements', require('./routes/announcementRoutes'));
 app.use('/api/notices', require('./routes/noticeRoutes'));
 app.use('/api/careers', require('./routes/careerRoutes'));
+// Add this with other routes
 app.use('/api/blogs', require('./routes/blogRoutes'));
 app.use('/api/admission', require('./routes/admissionRoutes')); // Admission route
 
